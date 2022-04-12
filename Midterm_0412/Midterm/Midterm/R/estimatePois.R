8c3b401b04df8dac75319984122292a69abfa54b
Berta D <berta.diaz.mtz@gmail.com>
2022-04-12 18:30:32 -0500

    fix estimatePois function
    Fix and add comments. Also I take out lambda of arguments

--- Midterm_0412/Midterm/Midterm/R/estimatePois.R
+++ Midterm_0412/Midterm/Midterm/R/estimatePois.R
@@ -1,31 +1,34 @@
-#' Title
+#' estimatePois
 #'
-#' @param y
-#' @param lambda
-#' @param SEtype
+#' Returns an object of class PoisMLE
+#'
+#' @param y numeric. A vector of observed data
+#' @param SEtype character. The method used to calculate the standard error: basic or bootstrap
 #' @param ...
 #'
+#' @author Berta Diaz
 #' @return
 #' @export
 #'
 #' @examples
+#' y_test = rpois(100,4)
+#' estimatePois(y_test, SEtype = "bootstrap")
+#'
 setGeneric( #Create a generic
   name = "estimatePois",
-  def = function(y,lambda, SEtype, ...)
+  def = function(y, SEtype, ...)
   {standardGeneric("estimatePois")}
 )
 
 #' @export
-setMethod(
+setMethod( #Create a function that return an object of class PoisMLE
   f = "estimatePois",
-  definition = function(y,lambda, SEtype, ...){
-    MLE <- mle(y)
-    LL <- logLik(y, lambda)
-    SE <- standardError(y, SEtype)
-    #return(new("PoisMLE", MLE=MLE, LL=LL, SE=SE, SEtype=SEtype))
-
-
-    }
+  definition = function(y, SEtype, ...){ #Here we are using functions that we already built.
+    MLE <- mle(y) #Maximum likelihood
+    LL <- logLik(y, MLE) #Log likelihood
+    SE <- standardError(y, SEtype) #Standard errors
+    return(new("PoisMLE", y = y,mle=MLE, ll=LL, se=SE, SEtype=SEtype)) #Return an object of class PoisMLE
+  }
 )
 
 
