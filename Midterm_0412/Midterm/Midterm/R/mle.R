d67bf447483cc82ee036834ab65a270de0e53cd8
Berta D <berta.diaz.mtz@gmail.com>
2022-04-12 17:36:22 -0500

    Update mle function
    Add coments, title and description

--- Midterm_0412/Midterm/Midterm/R/mle.R
+++ Midterm_0412/Midterm/Midterm/R/mle.R
@@ -1,26 +1,31 @@
-#' Title
+#' Maximum likelihood
 #'
-#' @param y
-#' @param ...
+#' Calculates the maximum likelihood estimator for lambda
+#'
+#' @param y numeric. The vector of observed data
+#'
+#' @author Berta Diaz
 #'
 #' @return
 #' @export
 #'
 #' @examples
+#' y=c(1,2,3,4)
+#' mle(y)
 setGeneric( #Create a generic
   name = "mle",
-  def = function(y, ...)
+  def = function(y)
   {standardGeneric("mle")}
 )
 
 #' @export
-setMethod( #Create the function to calculate the standard error
+setMethod( #Create the function to calculate the Maximum likelihood
   f = "mle",
-  definition = function(y, ...){
-    n <- length(y)
-    mle <- sum(y)/n
+  definition = function(y){#Number of observations
+    n <- length(y) #Number of observations
+    mle <- sum(y)/n #Sum of y over n
 
-    return(mle)
+    return(mle) #Return the mle
 
   }
 )
